c4c925d0b8f59de5c5ec7ab16726b788634c824c
Jukii <Marzzuki@gmail.com>
2026-06-30 16:55:48 +0700

    Update Config App & Update DB

--- database/niki_frozen.sql
+++ database/niki_frozen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2026 at 06:56 AM
+-- Generation Time: Jun 30, 2026 at 11:54 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -167,7 +167,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (11, '2026_06_11_050339_create_financial_reports_table', 9),
 (12, '2026_06_11_060618_create_personal_access_tokens_table', 10),
 (13, '2026_06_13_051857_create_settings_table', 11),
-(14, '2026_06_13_051956_create_stock_mutations_table', 11);
+(14, '2026_06_13_051956_create_stock_mutations_table', 11),
+(15, '2026_06_30_080547_add_image_to_products_table', 12),
+(16, '2026_06_30_090000_change_category_to_string_on_products_table', 13);
 
 -- --------------------------------------------------------
 
@@ -207,8 +209,9 @@ CREATE TABLE `personal_access_tokens` (
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
 (4, 'App\\Models\\User', 3, 'auth_token', 'ecbd860d17538d75be3d14b96f33922ec324075b5bd31f826214175ecc8fb615', '[\"*\"]', NULL, NULL, '2026-06-11 00:11:38', '2026-06-11 00:11:38'),
 (11, 'App\\Models\\User', 5, 'auth_token', 'edfd9f3e56a7b635f4c659bbd2bbe07d3041e38e39f6e0eaa79d7bccac6d7fbd', '[\"*\"]', NULL, NULL, '2026-06-11 01:28:59', '2026-06-11 01:28:59'),
-(29, 'App\\Models\\User', 2, 'auth_token', 'b6cefb43b4e4215674fbb83320f634f336495c3abfb45de55e1ebab0a373e7f7', '[\"*\"]', '2026-06-17 21:48:41', NULL, '2026-06-12 08:29:55', '2026-06-17 21:48:41'),
-(34, 'App\\Models\\User', 1, 'auth_token', '124a5352d250dc9ec3050057ba36f55a29aaa680085c9128957baffab12b6d59', '[\"*\"]', '2026-06-17 21:51:01', NULL, '2026-06-17 21:48:49', '2026-06-17 21:51:01');
+(41, 'App\\Models\\User', 6, 'auth_token', '962803694ce3c3aff02b95713eaac9682a67d2f837e44e3bf960146da325c814', '[\"*\"]', '2026-06-22 02:12:46', NULL, '2026-06-18 00:19:58', '2026-06-22 02:12:46'),
+(60, 'App\\Models\\User', 2, 'auth_token', 'a5b520706b155de400dfba6ffef8183e08290cc1de1c14b2392bf31001de5d2b', '[\"*\"]', '2026-06-23 01:56:58', NULL, '2026-06-23 01:56:51', '2026-06-23 01:56:58'),
+(63, 'App\\Models\\User', 1, 'auth_token', '88a9f9cadb57a2bf70910f2c68a4fd45f86e8ac8eba43e3efb5a54b8a981a192', '[\"*\"]', '2026-06-25 00:55:51', NULL, '2026-06-25 00:11:56', '2026-06-25 00:55:51');
 
 -- --------------------------------------------------------
 
@@ -219,7 +222,8 @@ INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `n
 CREATE TABLE `products` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(150) NOT NULL,
-  `category` enum('Frozen','Snack','Dessert','Minuman','Lainnya') NOT NULL DEFAULT 'Frozen',
+  `category` varchar(50) NOT NULL DEFAULT 'Frozen',
+  `image` varchar(255) DEFAULT NULL,
   `price` decimal(12,2) NOT NULL DEFAULT 0.00,
   `expired_date` date NOT NULL,
   `branch_id` bigint(20) UNSIGNED NOT NULL,
@@ -231,16 +235,17 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `category`, `price`, `expired_date`, `branch_id`, `created_at`, `updated_at`) VALUES
-(1, 'Nugget Ayam', 'Frozen', 35000.00, '2025-12-31', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(2, 'Sosis Solo', 'Frozen', 28000.00, '2025-11-30', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(4, 'Kentang Goreng', 'Frozen', 20000.00, '2025-12-20', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(6, 'Pizza Frozen', 'Frozen', 55000.00, '2025-12-31', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(7, 'Dimsum', 'Frozen', 25000.00, '2025-12-10', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(8, 'Cireng', 'Snack', 10000.00, '2026-01-01', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
-(9, 'Sosis Kontol', 'Frozen', 8500.00, '2026-07-01', 1, '2026-06-11 03:58:58', '2026-06-11 03:58:58'),
-(10, 'todd', 'Frozen', 5000.00, '2026-06-11', 1, '2026-06-11 04:22:58', '2026-06-11 04:22:58'),
-(11, 'ehemm', 'Frozen', 4555.00, '2026-06-17', 2, '2026-06-11 04:28:42', '2026-06-11 04:28:42');
+INSERT INTO `products` (`id`, `name`, `category`, `image`, `price`, `expired_date`, `branch_id`, `created_at`, `updated_at`) VALUES
+(1, 'Nugget Ayam', 'Frozen', NULL, 35000.00, '2025-12-31', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
+(2, 'Sosis Solo', 'Frozen', NULL, 28000.00, '2025-11-30', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
+(4, 'Kentang Goreng', 'Frozen', NULL, 20000.00, '2025-12-20', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
+(6, 'Fiesta Chicken Nugget', 'Frozen', 'products/fJLCRM34fNRj9kt6WzLrVsi3a2XbN0iLtpMxqWFh.png', 55000.00, '2027-11-17', 1, '2026-06-10 22:53:29', '2026-06-30 02:00:05'),
+(7, 'Dimsum', 'Frozen', NULL, 25000.00, '2025-12-10', 1, '2026-06-10 22:53:29', '2026-06-10 22:53:29'),
+(8, 'Cireng', 'Tepung', 'products/WBW4yVhhscYT1OeB4X0UZ6ugqihD99X5uJIs8ftx.jpg', 10000.00, '2026-07-16', 2, '2026-06-10 22:53:29', '2026-06-30 02:46:44'),
+(9, 'Sosis Pip', 'Frozen', NULL, 8500.00, '2026-07-01', 1, '2026-06-11 03:58:58', '2026-06-23 01:55:39'),
+(10, 'todd', 'Frozen', NULL, 5000.00, '2026-06-11', 1, '2026-06-11 04:22:58', '2026-06-11 04:22:58'),
+(11, 'Fiesta Crispy Wings', 'Frozen', 'products/l9m20BTA7Owa58Dsksi9m8hCSrDzK8gvGDMGxExZ.jpg', 4555.00, '2026-06-17', 2, '2026-06-11 04:28:42', '2026-06-30 02:18:50'),
+(12, 'Fiesta Crispy Wings', 'Frozen', 'products/KGK8CF0yrvUDlMzWMailS6F2wNenR8XB7bzQEBfR.jpg', 10000.00, '2027-07-07', 1, '2026-06-30 02:21:25', '2026-06-30 02:21:25');
 
 -- --------------------------------------------------------
 
@@ -262,10 +267,9 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('hppZbs3y8lLepjZ6wQLZiUzpI9WKueIVt5FnA2li', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiUG83UVVndGRDT2xZRGhnNmgzaExQeFJSV0cyVURqWFBKdXRZWExwdSI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1781161700),
-('hxAAZ3dFxLbT3nBWJc4WnuTjxb0ufmO4QgUkqKwg', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoibTR5cW1aZFRnYUkxeHc1aFdwTE12MVZnakh5TzByeHNCeG9yYnlFSiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1781152652),
-('JZrWwcajJOkP7WWyqigKWXWeZLVzcGUx7P0GuqWG', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiRExPcjBHd3pSYm5MWGlBM1U2ODQxc0FFcThjVGU1Z1paNHBTajY4QiI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1781328239),
-('PKu5SyLKmgOWbm1Z8J46dCFmbWhytNNyWVxvmj41', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiMGk1VDFFQTA1N3llMjdWZ3ozOXZXUDVKVkNkSERueDBISFNFcUJ3TyI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7Tjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1781165846);
+('4FjmTlLKjkYKE2GeQKAvNoOv3Lbl8mdnJFbdsEMS', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Code/1.126.0 Chrome/148.0.7778.97 Electron/42.2.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiVzBmYXZObDRSNmJoenBxN2RtRDJiZzVTTVFqNThIejFFd2d5ejR3eCI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7czo1OiJyb3V0ZSI7czo1OiJsb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1782807397),
+('ayPDDe4jqUhT80tFESwcSRde2678GW1f2EnsDk21', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiSmM3NklaUEFDUjE3c1UyTmFDbWpyc1lOTGh6aG9YU0lGeDJqbE1nWSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9vd25lci9kYXNoYm9hcmQiO3M6NToicm91dGUiO3M6MTU6Im93bmVyLmRhc2hib2FyZCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czo0OiJuYW1lIjtzOjE4OiJPd25lciBOaWNreSBGcm96ZW4iO3M6NDoicm9sZSI7czo1OiJvd25lciI7czo5OiJicmFuY2hfaWQiO047fQ==', 1782811949),
+('H8HExdSF3s3AAQ8SasaTO4irExNO4SiYFLufEmhW', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiY0lNR3RYNlY1MnpsZXo5dm5rR0RmOHlPUllxTDJUT1d1b1BlUElhayI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9vd25lci91c2VycyI7czo1OiJyb3V0ZSI7czoxMToib3duZXIudXNlcnMiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6NDoibmFtZSI7czoxODoiT3duZXIgTmlja3kgRnJvemVuIjtzOjQ6InJvbGUiO3M6NToib3duZXIiO3M6OToiYnJhbmNoX2lkIjtOO30=', 1782813235);
 
 -- --------------------------------------------------------
 
@@ -318,6 +322,19 @@ CREATE TABLE `shifts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `shifts`
+--
+
+INSERT INTO `shifts` (`id`, `user_id`, `branch_id`, `opening_cash`, `closing_cash`, `expected_cash`, `difference`, `total_sales`, `total_transactions`, `status`, `opened_at`, `closed_at`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, 10000.00, 10000.00, 10000.00, 0.00, 0.00, 0, 'tutup', '2026-06-18 01:55:10', '2026-06-18 01:57:12', '2026-06-18 01:55:10', '2026-06-18 01:57:12'),
+(2, 2, 1, 10000.00, 30000.00, 30000.00, 0.00, 20000.00, 1, 'tutup', '2026-06-18 02:00:00', '2026-06-18 03:00:12', '2026-06-18 02:00:00', '2026-06-18 03:00:12'),
+(3, 2, 1, 10000.00, 35000.00, 35000.00, 0.00, 25000.00, 1, 'tutup', '2026-06-18 03:01:34', '2026-06-18 03:02:32', '2026-06-18 03:01:34', '2026-06-18 03:02:32'),
+(4, 5, 1, 10000.00, 118000.00, 118000.00, 0.00, 108000.00, 1, 'tutup', '2026-06-30 01:02:09', '2026-06-30 01:03:16', '2026-06-30 01:02:09', '2026-06-30 01:03:16'),
+(5, 5, 1, 0.00, 63000.00, 63000.00, 0.00, 63000.00, 2, 'tutup', '2026-06-30 01:18:21', '2026-06-30 02:17:09', '2026-06-30 01:18:21', '2026-06-30 02:17:09'),
+(6, 5, 1, 0.00, 0.00, 0.00, 0.00, 0.00, 0, 'tutup', '2026-06-30 02:19:08', '2026-06-30 09:51:56', '2026-06-30 02:19:08', '2026-06-30 09:51:56'),
+(7, 5, 1, 0.00, 65000.00, 65000.00, 0.00, 65000.00, 1, 'tutup', '2026-06-30 09:52:06', '2026-06-30 09:52:48', '2026-06-30 09:52:06', '2026-06-30 09:52:48');
+
 -- --------------------------------------------------------
 
 --
@@ -338,15 +355,16 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`id`, `product_id`, `branch_id`, `quantity`, `min_stock`, `updated_at`) VALUES
-(1, 1, 1, 45, 10, '2026-06-10 22:53:29'),
-(2, 2, 1, 8, 10, '2026-06-10 22:53:29'),
-(4, 4, 1, 28, 10, '2026-06-10 22:53:29'),
-(6, 6, 1, 0, 10, '2026-06-10 22:53:29'),
-(7, 7, 1, 15, 10, '2026-06-10 22:53:29'),
-(8, 8, 1, 2, 10, '2026-06-11 04:27:16'),
+(1, 1, 1, 43, 10, '2026-06-10 22:53:29'),
+(2, 2, 1, 6, 10, '2026-06-10 22:53:29'),
+(4, 4, 1, 27, 10, '2026-06-10 22:53:29'),
+(6, 6, 1, 99, 10, '2026-06-10 22:53:29'),
+(7, 7, 1, 13, 10, '2026-06-10 22:53:29'),
+(8, 8, 2, 13, 10, '2026-06-11 04:27:16'),
 (9, 9, 1, 1000, 10, NULL),
 (10, 10, 1, 5, 10, NULL),
-(11, 11, 2, 2, 10, NULL);
+(11, 11, 2, 102, 10, '2026-06-30 02:20:06'),
+(12, 12, 1, 17, 10, NULL);
 
 -- --------------------------------------------------------
 
@@ -367,6 +385,13 @@ CREATE TABLE `stock_mutations` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `stock_mutations`
+--
+
+INSERT INTO `stock_mutations` (`id`, `product_id`, `branch_id`, `user_id`, `type`, `quantity`, `before_stock`, `after_stock`, `note`, `created_at`, `updated_at`) VALUES
+(1, 11, 2, 1, 'in', 100, 2, 102, NULL, '2026-06-30 02:20:06', '2026-06-30 02:20:06');
 
 -- --------------------------------------------------------
 
@@ -390,6 +415,18 @@ CREATE TABLE `transactions` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `invoice_number`, `user_id`, `branch_id`, `shift_id`, `total`, `payment`, `change_amount`, `status`, `sync_status`, `synced_at`, `created_at`, `updated_at`) VALUES
+(1, 'INV-20260618-0001', 2, 1, 2, 20000.00, 50000.00, 30000.00, 'sukses', 'tersinkronisasi', '2026-06-18 02:00:26', '2026-06-18 02:00:26', '2026-06-18 02:00:26'),
+(2, 'INV-20260618-0002', 2, 1, 3, 25000.00, 50000.00, 25000.00, 'sukses', 'tersinkronisasi', '2026-06-18 03:01:59', '2026-06-18 03:01:59', '2026-06-18 03:01:59'),
+(3, 'INV-20260630-0003', 5, 1, 4, 108000.00, 150000.00, 42000.00, 'sukses', 'tersinkronisasi', '2026-06-30 01:02:55', '2026-06-30 01:02:55', '2026-06-30 01:02:55'),
+(4, 'INV-20260630-0004', 5, 1, 5, 28000.00, 50000.00, 22000.00, 'sukses', 'tersinkronisasi', '2026-06-30 02:12:33', '2026-06-30 02:12:33', '2026-06-30 02:12:33'),
+(5, 'INV-20260630-0005', 5, 1, 5, 35000.00, 70000.00, 35000.00, 'sukses', 'tersinkronisasi', '2026-06-30 02:13:01', '2026-06-30 02:13:01', '2026-06-30 02:13:01'),
+(6, 'INV-20260630-0006', 5, 1, 7, 65000.00, 100000.00, 35000.00, 'sukses', 'tersinkronisasi', '2026-06-30 09:52:24', '2026-06-30 09:52:24', '2026-06-30 09:52:24');
+
 -- --------------------------------------------------------
 
 --
@@ -406,6 +443,22 @@ CREATE TABLE `transaction_details` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transaction_details`
+--
+
+INSERT INTO `transaction_details` (`id`, `transaction_id`, `product_id`, `qty`, `price_at_sale`, `subtotal`, `created_at`, `updated_at`) VALUES
+(1, 1, 8, 2, 10000.00, 20000.00, '2026-06-18 02:00:26', '2026-06-18 02:00:26'),
+(2, 2, 7, 1, 25000.00, 25000.00, '2026-06-18 03:01:59', '2026-06-18 03:01:59'),
+(3, 3, 1, 1, 35000.00, 35000.00, '2026-06-30 01:02:55', '2026-06-30 01:02:55'),
+(4, 3, 4, 1, 20000.00, 20000.00, '2026-06-30 01:02:55', '2026-06-30 01:02:55'),
+(5, 3, 2, 1, 28000.00, 28000.00, '2026-06-30 01:02:55', '2026-06-30 01:02:55'),
+(6, 3, 7, 1, 25000.00, 25000.00, '2026-06-30 01:02:55', '2026-06-30 01:02:55'),
+(7, 4, 2, 1, 28000.00, 28000.00, '2026-06-30 02:12:33', '2026-06-30 02:12:33'),
+(8, 5, 1, 1, 35000.00, 35000.00, '2026-06-30 02:13:01', '2026-06-30 02:13:01'),
+(9, 6, 6, 1, 55000.00, 55000.00, '2026-06-30 09:52:24', '2026-06-30 09:52:24'),
+(10, 6, 12, 1, 10000.00, 10000.00, '2026-06-30 09:52:24', '2026-06-30 09:52:24');
 
 -- --------------------------------------------------------
 
@@ -432,11 +485,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`, `branch_id`, `status`) VALUES
-(1, 'Owner Nicky Frozen', 'owner@nicksfrozen.com', NULL, '$2y$12$elwss0hHgNi7iLhPpVvslO9v7NIdoIs5eoCqyuBTsIjxsD.ZwZqge', 'owner', NULL, '2026-06-10 22:53:27', '2026-06-10 22:53:27', NULL, 'aktif'),
-(2, 'Siti Aisyah', 'siti@nicksfrozen.com', NULL, '$2y$12$rk7Myg1amAJ4trp2WmymEetzcKm7fk6aI1z.eKTn2wlqrRBxPb2CS', 'kasir', NULL, '2026-06-10 22:53:27', '2026-06-10 22:53:27', 1, 'aktif'),
+(1, 'Owner Nicky Frozen', 'owner@nicksfrozen.com', NULL, '$2y$12$elwss0hHgNi7iLhPpVvslO9v7NIdoIs5eoCqyuBTsIjxsD.ZwZqge', 'owner', 'lFrDypl3xYn7jIUbqJbS4J0Xlg70n2uAhH8XrUM7eASbmIaFw2d530MrXOhM', '2026-06-10 22:53:27', '2026-06-10 22:53:27', NULL, 'aktif'),
+(2, 'Siti Aisyah', 'siti@nicksfrozen.com', NULL, '$2y$12$rk7Myg1amAJ4trp2WmymEetzcKm7fk6aI1z.eKTn2wlqrRBxPb2CS', 'kasir', NULL, '2026-06-10 22:53:27', '2026-06-23 01:56:45', 2, 'aktif'),
 (3, 'Budi Santoso', 'budi@nicksfrozen.com', NULL, '$2y$12$.93gEZjRWkhSM5rlI3woouakW0w8VA0BkrXXESrYC1BkTtF/lA9d6', 'kasir', NULL, '2026-06-10 22:53:28', '2026-06-10 22:53:28', 2, 'aktif'),
-(4, 'Dewi Lestari', 'dewi@nicksfrozen.com', NULL, '$2y$12$MYYSWxDmmUSDKGEfpMcvSemvSfJnSqAJSi.b0SFh/dZ00WU/GoxPS', 'kasir', NULL, '2026-06-10 22:53:29', '2026-06-10 22:53:29', 1, 'nonaktif'),
-(5, 'Marjukii', 'ramadhanzaki@students.amikom.ac.id', NULL, '$2y$12$OQnjVZFrNX6EILliSecI9..yerVv.uHPU2JOT/PYCGwJLHBoc.2mK', 'kasir', NULL, '2026-06-11 01:28:47', '2026-06-11 01:28:47', 1, 'aktif');
+(5, 'Marjukii', 'ramadhanzaki@students.amikom.ac.id', NULL, '$2y$12$g1e7ZTJlFHWcpxvTswzwVeGWQJevrrq7hXkn3Xhc6FZnB654IIY7m', 'kasir', 'UmzywtcY4fSdNvMLrcflMoACcpAkDkKlaIdK8PDIbgL2g9rprOxnc1BBJn4e', '2026-06-11 01:28:47', '2026-06-30 00:55:31', 1, 'aktif'),
+(6, 'ronaldo', 'ronaldo@gmail.com', NULL, '$2y$12$zsoyEEKkYPmGahbPgB6CWeztogDnw0rdPuJ04dKCRp83xGRCIqqIm', 'kasir', NULL, '2026-06-18 00:18:34', '2026-06-18 00:18:34', 2, 'aktif');
 
 --
 -- Indexes for dumped tables
@@ -615,19 +668,19 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `settings`
@@ -639,37 +692,37 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `shifts`
 --
 ALTER TABLE `shifts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `stocks`
 --
 ALTER TABLE `stocks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `stock_mutations`
 --
 ALTER TABLE `stock_mutations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `transaction_details`
 --
 ALTER TABLE `transaction_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
